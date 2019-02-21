f3a45911f6cb34f2e75cb3162993bfd3915df12e
Cheryl K <cherylkelson@icloud.com>
2019-02-21 16:04:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,9 +7,11 @@ class App < Sinatra::Base
   end
 
   get '/hometown' do
-  "My hometown is Chicago, the Windy City."
+    "My hometown is Chicago, the Windy City."
 end
 
-get '/favorite-song' do
-  "My favorite song is "The Sound of Music." 
+  get '/favorite-song' do
+    "My favorite song is Abracadabra.""
+  end
+
 end
